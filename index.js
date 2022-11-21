d48006d9521a514cdbcdc44a2841cc2cca258734
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-11-21 18:37:26 +0100

    Creating our middlewears

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-import express from "express"
+import express from "express";
+import bodyParser from 'body-parser';
+import router from './Routes/users.js';
 
+const app = express();
+const PORT = 3000;
+
+// Middlewear
+
+app.use(bodyParser.json());
+
+app.use("/users", router);
+
+app.get("/", (req,res) => {
+    res.end("I love u alina <3 ");
+});
+
+// Start the server 
+
+app.listen(PORT, () => {
+    console.log(`Your application si running perfectly in http://localhost:${PORT}`);
+})
 
 
